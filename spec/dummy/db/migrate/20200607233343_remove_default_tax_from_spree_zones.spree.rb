88e30275c55ed6a738db8bc3efd47b39564614c8
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2024-01-08 21:34:05 -0600

    Remove spec/dummy files

--- spec/dummy/db/migrate/20200607233343_remove_default_tax_from_spree_zones.spree.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-# This migration comes from spree (originally 20170831201542)
-
-class RemoveDefaultTaxFromSpreeZones < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :spree_zones, :default_tax, :boolean, default: false
-  end
-end
